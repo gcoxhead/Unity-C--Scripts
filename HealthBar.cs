33b750e6f2812962e6e3850a38e81abfa392ea72
Gary Coxhead <79542013+gcoxhead@users.noreply.github.com>
2026-02-02 21:56:55 +0000

    Create HealthBar.cs
    Added health bar, need to implement damage/ hit points to test.

---
+++ HealthBar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    public Slider slider;
+
+    [SerializeField]
+    public Gradient gradient;
+
+    [SerializeField]
+    public Image fill;
+
+   
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+
+    }
+
+    public void SetHealth(int Health)
+    {
+        slider.value = Health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
+    }
+}
